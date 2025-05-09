b8a5fd665780448aa0a67846e8299088d4babe7b
Valdair <valdair3d@gmail.com>
2025-05-09 11:55:18 -0300

    Descrição das alterações

--- escritor-app-cra/src/styles/globalStyles.ts
+++ escritor-app-cra/src/styles/globalStyles.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
-import { Theme } from './theme';
 
-const GlobalStyles = createGlobalStyle<{ theme: Theme }>`
+const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap');
   
   * {
